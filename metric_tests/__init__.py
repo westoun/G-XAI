a756883b3395db9a879b5e2c5161836235b956b1
westoun <christoph.werner.stein@gmail.com>
2024-04-08 17:01:31 +0200

    Remove metric tests

--- metric_tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from .test_category_count_independence import run_category_count_independence_test
-from .test_range_independence import run_range_independence_test
-from .test_middle_value_independence import run_middle_value_independence_test
-from .test_type_independence import run_type_independence_test
-from .test_edge_case_behavior import run_edge_case_behavior_test
